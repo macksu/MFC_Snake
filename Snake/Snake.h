a729a9cb30476916e3a27ca0774cbb855540ccf0
macksu <1115486989@qq.com>
2024-01-02 19:17:37 +0800

    创建mfc贪吃蛇项目

---
+++ Snake/Snake.h
@@ -0,0 +1,32 @@
+﻿
+// Snake.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CSnakeApp:
+// 有关此类的实现，请参阅 Snake.cpp
+//
+
+class CSnakeApp : public CWinApp
+{
+public:
+	CSnakeApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CSnakeApp theApp;
